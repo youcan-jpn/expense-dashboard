63c802abd6eba95545e3c6325299d9f46ea5946c
yuta ono <youcana40627@gmail.com>
2022-09-07 11:10:55 +0900

    add: implementation of GET /receipts/{receipt_id} in flask

--- backend/flask/src/endpoints/receipts_by_id.py
+++ backend/flask/src/endpoints/receipts_by_id.py
@@ -4,6 +4,10 @@ import controllers
 
 
 class ReceiptsById(Resource):
+    def get(self, receipt_id):
+        ret = controllers.get_receipt_by_id(receipt_id=receipt_id)
+        return ret
+
     def delete(self, receipt_id):
         ret = controllers.delete_receipts_by_id(receipt_id=receipt_id)
         if ret:
